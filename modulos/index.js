f1ecc74923abdf90953759d29512274207357938
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-07-07 16:01:51 -0500

    estilos

--- modulos/index.js
+++ modulos/index.js
@@ -29,31 +29,23 @@ Api.prototype.Rec = async function(){
         console.log(pokedato);
     });
 } 
-Api.prototype.types = async function(types){
-    try{
-        let arr = types.map( async e =>{ 
-        try{
-            let data = await fetch(e.type.url);
-            return data.json();
-        }catch(err){
-            
-        }
-       
-    }); 
-    }catch(err){
-        console.log(err);
-    }
-    
+Api.prototype.types = function(types){
+    return types.map(e => `<span class = "${e.type.name}">${e.type.name}</span>`);
 }
 Api.prototype.generador = async function(pokedato){
-    let tipos = await this.types(pokedato.types);
-    console.log( tipos);
+    let tipos =  this.types(pokedato.types);
+    console.log(tipos);
     this.content.innerHTML += `
     <div class = "card">
+        
+        <img src = "${pokedato.sprites["other"]["official-artwork"]["front_default"]}" class= "imagen">
         <div class = "card-head">
+            N ${pokedato.order}
             <h2>${pokedato["name"]}</h2>
         </div>
-        <img src = "${pokedato.sprites["front_default"]}">
+        <div class = "habilidades">
+            ${tipos.join(" ")}
+        </div>
     </div>
     `;
 }
